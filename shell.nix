6e370b5c8fc8e5d84763ecd7730503466b047255
albertgoncalves <goncalves.albert@gmail.com>
2026-06-27 11:19:56 -0400

    init repo

---
+++ shell.nix
@@ -0,0 +1,16 @@
+with import <nixpkgs> {};
+mkShell {
+    buildInputs = [
+        (python3.withPackages (ps: with ps; [
+            black
+            flake8
+            matplotlib
+            pandas
+            scikit-learn
+            scipy
+            statsmodels
+        ]))
+    ];
+    shellHook = ''
+    '';
+}
